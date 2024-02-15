e2acbabab033a47ee138a65189d7ffa7ffa26777
Ayush Gupta <guptaayush212004@gmail.com>
2024-02-15 22:37:35 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
+import Vans from './components/Vans'
+import VanDetail from './components/VanDetail'
 import './App.css'
 
 function App() {
@@ -12,11 +14,14 @@ function App() {
                     <Link className="site-logo" to="/">#VanLife</Link>
                     <nav>
                         <Link to="/about">About</Link>
+                        <Link to="/vans">Vans</Link>
                     </nav>
                 </header>
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
+                    <Route path="/vans" element={<Vans />} />
+                    <Route path="/vans/:id" element={<VanDetail />} />
                 </Routes>
             </BrowserRouter>
         </>
